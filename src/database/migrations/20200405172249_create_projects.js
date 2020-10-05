02eb76705cc3e88fc86ec422dc995c52c416ea6f
Claiton <cdpaulus@inf.ufsm.br>
2020-10-04 22:03:16 -0300

    Migrations not found

--- src/database/migrations/20200405172249_create_projects.js
+++ src/database/migrations/20200405172249_create_projects.js
@@ -1,18 +1,19 @@
 
-exports.up = function(knex) {
-    return knex.schema.createTable('projects', function(table){
+exports.up = function (knex) {
+    return knex.schema.createTable('projects', function (table) {
         table.increments();
 
         table.string('title').notNullable();
         table.string('description').notNullable();
         table.string('holder');
 
+
         table.string('professor_id').notNullable();
         table.foreign('professor_id').references('id').inTable('professors');
     });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTable('projects');
 
 };

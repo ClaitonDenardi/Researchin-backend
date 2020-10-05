bfe61ce05aaa8b05855bc9fd6aef2684b2b4f4f6
Claiton <cdpaulus@inf.ufsm.br>
2020-10-04 21:10:20 -0300

    Try to change DB

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,9 @@
+const prodConfig = Object.assign(
+  {},
+  devConfig,
+  { client: 'pg', connection: process.env.DATABASE_URL }
+)
+
 module.exports = {
 
   development: {
@@ -5,42 +11,12 @@ module.exports = {
     connection: {
       filename: './src/database/db.sqlite'
     },
-      migrations:{
-        directory: './src/database/migrations'
-      },
-      useNullAsDefault: true,
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  production: prodConfig
 
 };

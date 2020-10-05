b04321f37de0e653184e8bc2b48e85ff325634c8
Claiton <cdpaulus@inf.ufsm.br>
2020-10-04 21:50:14 -0300

    update ++

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,8 @@
-const prodConfig = Object.assign(
-  {},
-  this.development,
-  { client: 'pg', connection: process.env.DATABASE_URL }
-)
+
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/db.sqlite'
-    },
-    migrations: {
-      directory: './src/database/migrations'
-    },
-    useNullAsDefault: true,
-  },
-  production: prodConfig
+    client: 'pg', connection: process.env.DATABASE_URL
+  }
 }

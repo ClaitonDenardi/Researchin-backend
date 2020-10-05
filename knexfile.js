9d66699cb14dd7fecf1b7f3e15b0c360db66e677
Claiton <cdpaulus@inf.ufsm.br>
2020-10-04 21:59:45 -0300

    ++

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,15 @@
-
-
 module.exports = {
 
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
   production: {
     client: 'pg', connection: process.env.DATABASE_URL
   }

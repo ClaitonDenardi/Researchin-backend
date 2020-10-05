89861b8f50c0afbd1db0a88c811fdef90d7f5752
Claiton <cdpaulus@inf.ufsm.br>
2020-10-04 21:43:40 -0300

    handmade try

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,17 +1,7 @@
-// const knex = require('knex');
-// const configuration = require('../../knexfile');
+const knex = require('knex');
+const configuration = require('../../knexfile');
 
-// const connection = knex(configuration.production);
+const connection = knex(configuration.production);
 
-// module.exports = connection;
+module.exports = connection;
 
-const dbConfigObj = require('../../knexfile')
-
-// .....
-
-const app = express()
-
-let dbConnectionConfig = dbConfigObj.production
-
-const appDb = connectToDb(dbConnectionConfig)
-Model.knex(appDb)

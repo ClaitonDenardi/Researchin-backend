c2ecfaf393ac39182f8956f8c569dfa1aba98262
Claiton <cdpaulus@inf.ufsm.br>
2020-10-04 21:16:45 -0300

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,3 +4,7 @@ const configuration = require('../../knexfile');
 const connection = knex(configuration.production);
 
 module.exports = connection;
+
+let dbConnectionConfig = connection;
+const appDb = connectToDb(dbConnectionConfig)
+Model.knex(appDb)
